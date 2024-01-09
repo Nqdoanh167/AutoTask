2b079587454152f1235bacaceb26f0bf41b56791
duonglong.tech <duonglong.dev@gmail.com>
2024-01-09 16:52:40 +0700

    Feat (Structure): Api config data

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,8 @@ export const environment = {
   module: 'auto-task',
   apiAddress: 'https://smax.app/api',
   apiModule: 'https://smax.app/api',
+  urlDomain: 'https://dev.smax.app',
+  clientUrl: 'https://dev.smax.app',
 };
 
 const parsedURL = new URL(location.href);
@@ -10,4 +12,6 @@ if (parsedURL.hostname !== 'localhost') {
   const serviceAddr = `${parsedURL.origin}/api`;
   environment.apiAddress = serviceAddr;
   environment.apiModule = serviceAddr;
+  environment.urlDomain = parsedURL.origin;
+  environment.clientUrl = parsedURL.origin;
 }
