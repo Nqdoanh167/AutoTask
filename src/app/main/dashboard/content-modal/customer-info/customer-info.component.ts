f264330574bd8ff3023cce93103d50f9c05b8e0f
duonglong.tech <duonglong.dev@gmail.com>
2024-01-29 11:56:13 +0700

    Feat (Structure): Dashboard Task

--- src/app/main/dashboard/content-modal/customer-info/customer-info.component.ts
+++ src/app/main/dashboard/content-modal/customer-info/customer-info.component.ts
@@ -1,8 +1,23 @@
 import {Component, Input, OnDestroy, OnInit} from '@angular/core';
-import {Subject} from 'rxjs';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  finalize,
+  Subject,
+  takeUntil,
+} from 'rxjs';
 import {AbstractControl, FormGroup} from '@angular/forms';
 import {ApiLocationService} from '@app/services/api/location';
 import {IDistrict, IProvince, IWard} from '@app/types/location';
+import {
+  Combo,
+  Customer,
+  ICommonDataLazy,
+  IQueryBase,
+} from '@app/types/viewmodels';
+import {CustomerService} from '@app/services/api/customer.service';
+import {uniqBy} from 'lodash';
+import {CommonService} from '@app/services/common/common.service';
 
 @Component({
   selector: 'app-customer-info',
@@ -18,8 +33,28 @@ export class CustomerInfoComponent implements OnDestroy, OnInit {
   public listProvince: IProvince[] = [];
   public listDistrict: IDistrict[] = [];
   public listWard: IWard[] = [];
+  public trigger = {
+    name: false,
+  };
+  protected input$ = new Subject<string>();
 
-  constructor(private readonly apiLocationService: ApiLocationService) {}
+  public selectedCustomer: Customer | null = null;
+  public customers: ICommonDataLazy<Customer, IQueryBase> = {
+    rows: [],
+    loading: false,
+    paramsQuery: {
+      page: 1,
+      limit: 20,
+      sort: '-createdAt',
+    },
+    isAllowLoadMore: false,
+  };
+
+  constructor(
+    private readonly apiLocationService: ApiLocationService,
+    private readonly customerService: CustomerService,
+    private readonly commonService: CommonService,
+  ) {}
 
   get f(): {[key: string]: AbstractControl} {
     return this.formGroup.controls;
@@ -33,6 +68,57 @@ export class CustomerInfoComponent implements OnDestroy, OnInit {
     if (this.f['districtCode'].value) {
       this.getWard(this.f['provinceCode'].value, this.f['districtCode'].value);
     }
+    this.input$
+      .pipe(debounceTime(400), distinctUntilChanged())
+      .subscribe((data) => {
+        this.customers.paramsQuery = {
+          ...this.customers.paramsQuery,
+          q: data,
+        };
+        this.getListCustomer(true, true);
+      });
+  }
+
+  getListCustomer(isInit: boolean = false, isSearching: boolean = false) {
+    this.customers.loading = true;
+    let oldData: any = [];
+    const ids: string[] = [];
+    const query = {
+      ...this.customers.paramsQuery,
+      ...(isInit && ids.length && {ids: ids}),
+    };
+    if (isSearching) {
+      oldData = [...this.customers.rows];
+      this.customers.rows = [];
+    }
+
+    this.customerService.customer
+      .get(query)
+      .pipe(
+        takeUntil(this.destroy$),
+        finalize(() => (this.customers.loading = false)),
+      )
+      .subscribe({
+        next: (res) => {
+          if (res && res.status === 200) {
+            let newData: Customer[] = [];
+            if (isSearching) {
+              newData = [...res.data, ...oldData];
+            } else {
+              newData = [...this.customers.rows, ...res.data];
+            }
+            this.customers.rows = uniqBy(newData, 'id');
+            this.customers.isAllowLoadMore = true;
+          } else {
+            this.customers.isAllowLoadMore = false;
+            this.commonService.handleResErr(res);
+          }
+        },
+        error: (err) => {
+          this.customers.isAllowLoadMore = false;
+          this.commonService.handleResErr(err);
+        },
+      });
   }
 
   getProvince() {
@@ -126,6 +212,56 @@ export class CustomerInfoComponent implements OnDestroy, OnInit {
         return;
     }
   }
+
+  handleBlur($event: any) {
+    let value = $event?.target?.value;
+    console.log('=>(customer-info.component.ts:133) value', value);
+    // if (!value) return;
+    // const currentValue = this.formItem.value.values;
+    // this.formItem.patchValue({
+    //   values: [...currentValue, value],
+    // });
+    // this.selectMultiple!.searchTerm = '';
+  }
+
+  handleChooseCustomer(customer: any) {
+    this.trigger.name = false;
+    this.selectedCustomer = customer;
+    this.formGroup.patchValue({
+      name: customer.name,
+      phone: customer.phone,
+      email: customer.email,
+      province: customer.province,
+      provinceCode: customer.provinceCode,
+      district: customer.district,
+      districtCode: customer.districtCode,
+      ward: customer.ward,
+      wardCode: customer.wardCode,
+      address: customer.address,
+    });
+  }
+
+  onSearch(event?: any) {
+    this.trigger.name = true;
+    this.input$.next(event?.target?.value);
+  }
+
+  handleClearSelectedCustomer() {
+    this.selectedCustomer = null;
+    this.formGroup.patchValue({
+      name: null,
+      phone: null,
+      email: null,
+      province: null,
+      provinceCode: null,
+      district: null,
+      districtCode: null,
+      ward: null,
+      wardCode: null,
+      address: null,
+    });
+  }
+
   ngOnDestroy(): void {
     this.destroy$.next(true);
     this.destroy$.complete();
