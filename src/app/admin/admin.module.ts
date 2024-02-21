a58b92bcaacf8b0b398bd2fa996d670a808d70e6
duonglong.tech <duonglong.dev@gmail.com>
2024-02-21 15:49:52 +0700

    Feat (UI): Source site

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,8 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {AdminComponent} from './admin.component';
 import {AdminRoutingModule} from './admin-routing.module';
-import {HeaderModule} from '../share/header/header.module';
-// import { SidebarAdminModule } from 'src/app/share/sidebar-admin/sidebar-admin.module';
+import {HeaderModule} from '@share/layout/header/header.module';
 
 @NgModule({
   declarations: [AdminComponent],
