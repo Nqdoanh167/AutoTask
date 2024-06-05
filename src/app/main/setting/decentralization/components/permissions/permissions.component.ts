228352de31a34cd62d705e80e1fa0c99f0012ef1
duonglong.tech <duonglong.dev@gmail.com>
2024-06-05 11:09:33 +0700

    Feat (UI): permissions

--- src/app/main/setting/decentralization/components/permissions/permissions.component.ts
+++ src/app/main/setting/decentralization/components/permissions/permissions.component.ts
@@ -8,22 +8,26 @@ import {
 import {Subject, takeUntil} from 'rxjs';
 import {removeCharacter} from '@app/utils/common';
 import {AuthService} from '@app/services/api/auth.service';
-import {IBranch} from '@app/types/viewmodels';
+import {IBranch, IQueryBase} from '@app/types/viewmodels';
 import {BsModalService} from 'ngx-bootstrap/modal';
+import {StandardTableComponent} from '@share/common/standard-table/standard-table.component';
 
 @Component({
   selector: 'app-permissions',
   templateUrl: './permissions.component.html',
   styleUrls: ['./permissions.component.scss'],
 })
-export class PermissionsComponent implements OnDestroy, OnInit {
-  public configFilters: IFilterTopTable[] = [
+export class PermissionsComponent
+  extends StandardTableComponent<any, IQueryBase>
+  implements OnDestroy, OnInit
+{
+  public override configFilters: IFilterTopTable[] = [
     {
       type: ETypeFilter.SEARCH,
       placeholder: 'Tìm theo...',
     },
   ];
-  public configButtons: IFilterTopButton[] = [
+  public override configButtons: IFilterTopButton[] = [
     {
       name: 'add_new',
       type: ETypeButton.PRIMARY,
@@ -35,14 +39,12 @@ export class PermissionsComponent implements OnDestroy, OnInit {
     data: false,
   };
 
-  public dataSource: IBranch[] = [];
-  public listFilteredBranches: IBranch[] = [];
-
   private destroy$ = new Subject();
   constructor(
     private readonly authService: AuthService,
     private readonly modalService: BsModalService,
   ) {
+    super();
     this.authService.currentBiz
       .pipe(takeUntil(this.destroy$))
       .subscribe((biz) => {});
@@ -50,24 +52,13 @@ export class PermissionsComponent implements OnDestroy, OnInit {
 
   ngOnInit() {}
 
-  handleAction(name: string) {
+  override handleAction(name: string) {
     if (name === 'add_new') {
       this.handleUpdate();
     }
   }
 
-  onSearch(value: {term: string; name: string}) {
-    const {term} = value;
-    const keyword = removeCharacter(term)
-      .toLocaleLowerCase()
-      .replace(/[ ]+/, ' ');
-    this.listFilteredBranches = this.dataSource.filter(
-      (user) =>
-        !keyword ||
-        (user.name &&
-          removeCharacter(user.name).toLocaleLowerCase().indexOf(keyword) > -1),
-    );
-  }
+  override getDataSource(isReset?: boolean) {}
 
   handleUpdate(data?: any) {}
 
---
+++ src/app/share/common/standard-table/standard-table.component.ts
@@ -0,0 +1,111 @@
+import {ChangeDetectorRef, Component, inject, Input} from '@angular/core';
+import {ICommonDataSource} from '@app/types/viewmodels';
+import {sortBy, sortIcon} from '@app/utils/common';
+import {FilterTopTableComponent} from '@share/common/filter-top-table/filter-top-table.component';
+import {ConvertTypeModule} from '@share/pipe/convertType/convertType.module';
+import {PaginationModule} from 'ngx-bootstrap/pagination';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {IFilterTopButton, IFilterTopTable} from '@app/types/common';
+import {CommonModule} from '@angular/common';
+
+@Component({
+  selector: 'app-standard-table',
+  standalone: true,
+  imports: [
+    FilterTopTableComponent,
+    ConvertTypeModule,
+    PaginationModule,
+    ReactiveFormsModule,
+    FormsModule,
+    CommonModule,
+  ],
+  templateUrl: './standard-table.component.html',
+  styleUrl: './standard-table.component.scss',
+})
+export class StandardTableComponent<T, K extends any> {
+  @Input() isHidePaginate: boolean = false;
+
+  protected detect = inject(ChangeDetectorRef);
+
+  protected sortProperty: string = 'createdAt';
+  protected sortOrder = 1;
+
+  public configFilters: IFilterTopTable[] = [];
+  public configButtons: IFilterTopButton[] = [];
+
+  public item: ICommonDataSource<T, K | any> = {
+    rows: [],
+    loading: false,
+    paramsQuery: {
+      limit: 20,
+      page: 1,
+      q: '',
+      sort: '-createdAt',
+    },
+    total: 0,
+  };
+
+  constructor() {}
+
+  handleAction(name: string) {}
+
+  getDataSource(isReset?: boolean) {}
+
+  onSearch(value: {term: string; name: string}) {
+    const {term} = value;
+    this.item.paramsQuery.q = term;
+    this.getDataSource(true);
+  }
+
+  handleSortBy(property: string): void {
+    const {sortProperty, sortOrder, sortQuery} = sortBy(
+      this.sortOrder,
+      this.sortProperty,
+      property,
+    );
+    [this.sortProperty, this.sortOrder] = [sortProperty, sortOrder];
+    this.item.paramsQuery = {
+      ...this.item.paramsQuery,
+      sort: sortQuery ? sortQuery : undefined,
+    };
+  }
+
+  handleSortIcon(property: string) {
+    return sortIcon(property, this.sortProperty, this.sortOrder);
+  }
+
+  pageChanged(event: {page?: number; itemsPerPage?: number}, limit: any): void {
+    if (event.page) {
+      this.item.paramsQuery = {...this.item.paramsQuery, page: event.page};
+    }
+    if (limit?.target?.value) {
+      this.item.paramsQuery = {
+        ...this.item.paramsQuery,
+        limit: Number(limit?.target?.value || 10),
+        page: 1,
+      };
+    }
+    this.getDataSource();
+  }
+
+  onSelectFilter(data: {value?: string; name: string}) {
+    try {
+      const {value, name} = data;
+      const filter = this.item.paramsQuery?.filter || '{}';
+      let obj = JSON.parse(filter);
+      if (!value?.length) {
+        delete obj[name];
+      } else {
+        if (value || Number(value) === 0) {
+          obj[name] = value;
+        } else {
+          delete obj[name];
+        }
+      }
+      this.item.paramsQuery.filter = JSON.stringify(obj);
+      this.getDataSource(true);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
