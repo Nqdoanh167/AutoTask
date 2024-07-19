cd214b2466c08f1cfc6071ef8a62e74655893f74
duonglong.tech <duonglong.dev@gmail.com>
2024-07-19 11:31:24 +0700

    Feat (UI): feat incoming call

--- src/app/services/common/phone-call.service.ts
+++ src/app/services/common/phone-call.service.ts
@@ -1,12 +1,27 @@
 import {Injectable} from '@angular/core';
 import {BehaviorSubject} from 'rxjs';
 import {IncomingCall} from '@app/types/call';
+import {
+  ManageMappingPhone,
+  StringeeReceiveCallEvent,
+} from '@app/types/sms-ott-call';
+import {StringeeClient} from 'stringee';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PhoneCallService {
   private incomingCallObj = new BehaviorSubject<IncomingCall | null>(null);
+
+  public connectLoading$ = new BehaviorSubject(false);
+  public connectedPhone$ = new BehaviorSubject<ManageMappingPhone | undefined>(
+    undefined,
+  );
+
+  protected stringeeClient: any;
+  protected call: any;
+  protected authenticatedWithUserId: any;
+
   constructor() {}
 
   getIncomingCall() {
@@ -16,4 +31,111 @@ export class PhoneCallService {
   setIncomingCall(incomingCall: IncomingCall | null) {
     this.incomingCallObj.next(incomingCall);
   }
+
+  settingCallEvents(call1: any) {
+    call1.on('error', (info: any) => {
+      console.log('on error: ' + JSON.stringify(info));
+    });
+
+    call1.on('addlocalstream', (stream: any) => {
+      console.log('on addlocalstream', stream);
+    });
+
+    call1.on('addremotestream', (stream: any) => {
+      console.log('on addremotestream', stream);
+    });
+
+    call1.on('signalingstate', (state: any) => {
+      console.log('signalingstate ', state);
+    });
+
+    call1.on('mediastate', (state: any) => {
+      console.log('mediastate ', state);
+    });
+
+    call1.on('info', (info: any) => {
+      console.log('on info', info);
+    });
+
+    call1.on('otherdevice', (data: any) => {
+      console.log('on otherdevice', data);
+    });
+  }
+
+  settingClientEvents() {
+    this.stringeeClient.on('connect', () => {
+      console.log('connected to StringeeServer');
+    });
+
+    this.stringeeClient.on('authen', (res: any) => {
+      console.log('on authen: ', res);
+      this.connectLoading$.next(false);
+      if (res.r === 0) {
+        this.authenticatedWithUserId = res.userId;
+      } else {
+        console.log('authen error: ', res);
+        this.connectedPhone$.next(undefined);
+      }
+    });
+
+    this.stringeeClient.on('disconnect', () => {
+      console.log('disconnected');
+    });
+
+    this.stringeeClient.on(
+      'incomingcall',
+      (incomingcall: StringeeReceiveCallEvent) => {
+        console.log('incomingcall: ', incomingcall);
+        this.call = incomingcall;
+        this.settingCallEvents(incomingcall);
+        const incomingCallObj: IncomingCall = {
+          from: incomingcall.fromNumber,
+          to: incomingcall.toNumber,
+        };
+        this.setIncomingCall(incomingCallObj);
+      },
+    );
+
+    this.stringeeClient.on('requestnewtoken', () => {
+      console.log(`request new token;
+            please get new access_token from YourServer
+            and call client.connect(new_access_token)`);
+    });
+
+    this.stringeeClient.on('otherdeviceauthen', (data: any) => {
+      console.log('otherdeviceauthen: ', data);
+    });
+  }
+
+  loginStringee(token: string) {
+    this.stringeeClient = new StringeeClient();
+    this.settingClientEvents();
+    this.stringeeClient.connect(token);
+  }
+
+  handleReject() {
+    this.callStopped();
+    this.call.reject((res: any) => {
+      console.log('reject res', res);
+    });
+  }
+
+  handleAnswer() {
+    this.call.answer((res: any) => {
+      console.log('answer res', res);
+    });
+  }
+
+  handleHangup() {
+    try {
+      this.callStopped();
+      this.call?.hangup((res: any) => {
+        console.log('hangup res', res);
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  callStopped() {}
 }
--- src/app/share/common/connect-phone/connect-phone.component.ts
+++ src/app/share/common/connect-phone/connect-phone.component.ts
@@ -5,18 +5,12 @@ import {CommonService} from '@app/services/common/common.service';
 import {ToastrService} from 'ngx-toastr';
 import {VOICE_PLATFORMS} from '@app/utils/variables';
 import {ICommonDataSource, IQueryBase} from '@app/types/viewmodels';
-import {
-  EVoicePlatform,
-  ManageMappingPhone,
-  StringeeReceiveCallEvent,
-} from '@app/types/sms-ott-call';
+import {EVoicePlatform, ManageMappingPhone} from '@app/types/sms-ott-call';
 import {finalize, takeUntil} from 'rxjs';
 import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
 import {NgIf} from '@angular/common';
 import {TooltipModule} from 'ngx-bootstrap/tooltip';
-import {StringeeClient} from 'stringee';
 import {PhoneCallService} from '@app/services/common/phone-call.service';
-import {IncomingCall} from '@app/types/call';
 
 @Component({
   selector: 'app-connect-phone',
@@ -42,9 +36,6 @@ export class ConnectPhoneComponent
   protected hasPermitSmsOttCall = false;
   protected connectedPhone: ManageMappingPhone | undefined;
   protected platforms = VOICE_PLATFORMS;
-  protected stringeeClient: any;
-  protected call: any;
-  protected authenticatedWithUserId: any;
 
   constructor(
     private readonly smsOttCallService: SmsOttCallService,
@@ -62,6 +53,16 @@ export class ConnectPhoneComponent
     if (this.hasPermitSmsOttCall) {
       this.getUserPhones();
     }
+    this.phoneCallService.connectLoading$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((res) => {
+        this.loading.connectPhone = res;
+      });
+    this.phoneCallService.connectedPhone$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((res) => {
+        this.connectedPhone = res;
+      });
   }
 
   getVoicePlatform(platform: EVoicePlatform) {
@@ -87,98 +88,15 @@ export class ConnectPhoneComponent
       });
   }
 
-  settingCallEvents(call1: any) {
-    call1.on('error', (info: any) => {
-      console.log('on error: ' + JSON.stringify(info));
-    });
-
-    call1.on('addlocalstream', (stream: any) => {
-      console.log('on addlocalstream', stream);
-    });
-
-    call1.on('addremotestream', (stream: any) => {
-      console.log('on addremotestream', stream);
-    });
-
-    call1.on('signalingstate', (state: any) => {
-      console.log('signalingstate ', state);
-    });
-
-    call1.on('mediastate', (state: any) => {
-      console.log('mediastate ', state);
-    });
-
-    call1.on('info', (info: any) => {
-      console.log('on info', info);
-    });
-
-    call1.on('otherdevice', (data: any) => {
-      console.log('on otherdevice', data);
-    });
-  }
-
-  settingClientEvents() {
-    this.stringeeClient.on('connect', () => {
-      console.log('connected to StringeeServer');
-    });
-
-    this.stringeeClient.on('authen', (res: any) => {
-      console.log('on authen: ', res);
-      this.loading.connectPhone = false;
-      if (res.r === 0) {
-        this.authenticatedWithUserId = res.userId;
-      } else {
-        this.connectedPhone = undefined;
-        console.log('authen error: ', res);
-      }
-    });
-
-    this.stringeeClient.on('disconnect', () => {
-      console.log('disconnected');
-      this.loading.connectPhone = false;
-      this.connectedPhone = undefined;
-    });
-
-    this.stringeeClient.on(
-      'incomingcall',
-      (incomingcall: StringeeReceiveCallEvent) => {
-        console.log('incomingcall: ', incomingcall);
-        this.call = incomingcall;
-        this.settingCallEvents(incomingcall);
-        const incomingCallObj: IncomingCall = {
-          from: incomingcall.fromNumber,
-          to: incomingcall.toNumber,
-        };
-        this.phoneCallService.setIncomingCall(incomingCallObj);
-      },
-    );
-
-    this.stringeeClient.on('requestnewtoken', () => {
-      console.log(`request new token;
-            please get new access_token from YourServer
-            and call client.connect(new_access_token)`);
-    });
-
-    this.stringeeClient.on('otherdeviceauthen', (data: any) => {
-      console.log('otherdeviceauthen: ', data);
-    });
-  }
-
-  loginStringee(token: string) {
-    this.stringeeClient = new StringeeClient();
-    this.settingClientEvents();
-    this.stringeeClient.connect(token);
-  }
-
   getTokenStringee(platformId: string) {
-    this.loading.connectPhone = true;
+    this.phoneCallService.connectLoading$.next(true);
     this.smsOttCallService.platform
       .getTokenReceiveCall(platformId)
       .pipe(takeUntil(this.destroy$))
       .subscribe({
         next: (res) => {
           if (res.status === 200 && res.data.token) {
-            this.loginStringee(res.data.token);
+            this.phoneCallService.loginStringee(res.data.token);
           } else {
             this.commonService.handleResErr(res);
           }
@@ -189,7 +107,7 @@ export class ConnectPhoneComponent
   handleConnectToPhone(data: ManageMappingPhone) {
     if (data.platform.platform === EVoicePlatform.STRINGEE) {
       if (data.platform?.id) {
-        this.connectedPhone = data;
+        this.phoneCallService.connectedPhone$.next(data);
         this.getTokenStringee(data.platform.id);
       } else {
         this.toarstService.warning('Không tìm thấy ID của nền tảng!');
