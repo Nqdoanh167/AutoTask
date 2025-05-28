36827d6aeebb6994dde855e247d5eee57cd5f617
VanCuong <googlythattuyet@gmail.com>
2025-05-28 14:13:50 +0700

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   module: 'auto-task',
   apiAddress: 'https://dev.smax.app/api',
-  // apiAddress: 'http://localhost:5020/api',
-  // apiModule: 'http://localhost:5020/api',
-  apiModule: 'https://dev.smax.app/api',
+  // apiAddress: 'http://localhost:7200/api',
+  apiModule: 'http://localhost:7200/api',
+  // apiModule: 'https://dev.smax.app/api',
   urlDomain: 'https://dev.smax.app',
   clientUrl: 'https://dev.smax.app',
 };
