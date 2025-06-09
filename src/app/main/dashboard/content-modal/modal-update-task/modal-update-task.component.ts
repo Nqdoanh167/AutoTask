102bd9b37b3aeafb6c28cd8342ba15bf1074ff6e
Nguyễn Quốc Doanh - timviec365 <112739154+Nqdoanh167@users.noreply.github.com>
2025-06-09 17:26:28 +0700

    update

--- src/app/main/dashboard/content-modal/modal-update-task/modal-update-task.component.ts
+++ src/app/main/dashboard/content-modal/modal-update-task/modal-update-task.component.ts
@@ -909,11 +909,11 @@ export class ModalUpdateTaskComponent
   handleChangeUnit(value: TreeNodeSelectEvent | TreeNodeUnSelectEvent) {
     const node = value.node as ModifiedUserUnit;
     this.getInfoUnit(node?.team || node?.department || node?.id);
-    this.formTeams.controls?.forEach((form) => {
-      form.patchValue({
-        userId: null,
-      });
-    });
+    // this.formTeams.controls?.forEach((form) => {
+    //   form.patchValue({
+    //     userId: null,
+    //   });
+    // });
   }
 
   preventUnselect(value: TreeNodeUnSelectEvent) {
