b8e01131b80f08d48150d7b96a291936fe0d26ce
Nguyễn Quốc Doanh - timviec365 <112739154+Nqdoanh167@users.noreply.github.com>
2025-06-10 15:50:53 +0700

    (fix) fix bug

--- src/app/main/dashboard/content-modal/modal-update-task/modal-update-task.component.ts
+++ src/app/main/dashboard/content-modal/modal-update-task/modal-update-task.component.ts
@@ -136,22 +136,6 @@ export class ModalUpdateTaskComponent
       this.patchForm();
     }
     this.handleCheckPermission();
-    if (this.sourceData) {
-    } else {
-      let branch = this.autoTaskService.getFirstUnit();
-      if (this.currentActiveViewMode?.options?.branchIds) {
-        const branchUnit = this.autoTaskService.getFirstUnitByIds(
-          this.currentActiveViewMode.options.branchIds,
-        );
-        if (branchUnit) {
-          branch = branchUnit;
-        }
-      }
-      this.updateForm.patchValue({
-        branch,
-      } as any);
-      this.getInfoUnit(branch?.team || branch?.department || branch?.id);
-    }
     if (this.code) {
       this.getTaskByCode();
     } else {
